ff749586da991f369fbeea5122dea7d07762bdfa
LAPTOP-9Q9LLP1O\juaz8 <juaz8888@gmail.com>
2022-01-19 20:01:07 -0500

    segundo commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import sqlite3
 from flask.templating import render_template
 
 
@@ -6,10 +7,22 @@ app=Flask(__name__)
 
 @app.route("/")
 def hola():
-    return render_template("main.html",nombre="Juan Diaz")
+    
+    conn=sqlite3.connect("base.db")
+    query="SELECT * FROM registro"
+    cur=conn.cursor()
+
+    cur.execute(query)
+
+    dato=cur.fetchall()
+
+    print(dato)
+
+    return render_template("main.html",nombre=dato)
 
 
 if __name__=='__main__':
-    print("hola")
+    
+
     app.run(port=5000,host="0.0.0.0",debug=True)
 
